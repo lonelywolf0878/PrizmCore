91c615657fc30f1e4aa491d2ac924f033be54522
prizmspace <terminator.t1000.good@gmail.com>
2020-09-05 19:33:17 +0300

    Fixed NPE

--- src/main/java/prizm/BlockImpl.java
+++ src/main/java/prizm/BlockImpl.java
@@ -65,10 +65,6 @@ final class BlockImpl implements Block {
 
     BlockImpl(int version, int timestamp, long previousBlockId, long totalAmountNQT, long totalFeeNQT, int payloadLength, byte[] payloadHash,
             byte[] generatorPublicKey, byte[] generationSignature, byte[] blockSignature, byte[] previousBlockHash, List<TransactionImpl> transactions) {
-        BlockImpl block = BlockchainImpl.getInstance().getBlock(previousBlockId);
-        if (block != null) {
-            this.baseTarget = Constants.getINITIAL_BASE_TARGET(block.getHeight()+1);
-        }
         this.version = version;
         this.timestamp = timestamp;
         this.previousBlockId = previousBlockId;
